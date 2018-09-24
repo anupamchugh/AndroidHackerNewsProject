0469bbe9a472a3102daf90dd4453dd7e359e9c21
Anupam Chugh <anupam.chugh@gmail.com>
2018-09-24 18:49:44 +0530

    .gitignore is now working

--- app/src/main/java/com/anupamchugh/androidhackernewsproject/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.anupamchugh.androidhackernewsproject;
-
-public class Constants {
-
-    public static final String FIREBASE_URL = "INCLUDE YOUR FIREBASE URL HERE";
-    public static final String FIREBASE_LOCATION_USERS = "users";
-    public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
-    public static final String FIREBASE_PROPERTY_TIMESTAMP = "timestamp";
-}
